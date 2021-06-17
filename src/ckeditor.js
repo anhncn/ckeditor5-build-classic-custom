351a73a637b3f3eff8158bff6fd54ef6b3fc6fb8
anhncn <nguyencongngocanh16071997@gmail.com>
2021-06-17 07:12:44 +0700

    math type

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -10,103 +10,142 @@
 // The editor creator to use.
 import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
 
-import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
-import UploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter";
-import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
-import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
-import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
-import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
-import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
-import EasyImage from "@ckeditor/ckeditor5-easy-image/src/easyimage";
-import Heading from "@ckeditor/ckeditor5-heading/src/heading";
-import Image from "@ckeditor/ckeditor5-image/src/image";
-import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
-import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
-import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
-import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
-import Indent from "@ckeditor/ckeditor5-indent/src/indent";
-import Link from "@ckeditor/ckeditor5-link/src/link";
-import List from "@ckeditor/ckeditor5-list/src/list";
-import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
-import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
-import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
-import Table from "@ckeditor/ckeditor5-table/src/table";
-import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
-import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
-import CloudServices from "@ckeditor/ckeditor5-cloud-services/src/cloudservices";
+// import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+// import UploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter";
+// import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
+// import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+// import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+// import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
+// import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
+// import EasyImage from "@ckeditor/ckeditor5-easy-image/src/easyimage";
+// import Heading from "@ckeditor/ckeditor5-heading/src/heading";
+// import Image from "@ckeditor/ckeditor5-image/src/image";
+// import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
+// import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
+// import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
+// import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
+// import Indent from "@ckeditor/ckeditor5-indent/src/indent";
+// import Link from "@ckeditor/ckeditor5-link/src/link";
+// import List from "@ckeditor/ckeditor5-list/src/list";
+// import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
+// import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+// import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
+// import Table from "@ckeditor/ckeditor5-table/src/table";
+// import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
+// import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
+// import CloudServices from "@ckeditor/ckeditor5-cloud-services/src/cloudservices";
 
-// import MathType from "@wiris/mathtype-ckeditor5";
+import MathType from "@wiris/mathtype-ckeditor5";
 
-import InsertImage from "./custom/InsertImage";
+// import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
+
+// import Code from "@ckeditor/ckeditor5-basic-styles/src/code.js";
+// import FontBackgroundColor from "@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js";
+// import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor.js";
+// import FontFamily from "@ckeditor/ckeditor5-font/src/fontfamily.js";
+// import FontSize from "@ckeditor/ckeditor5-font/src/fontsize.js";
+// import Highlight from "@ckeditor/ckeditor5-highlight/src/highlight.js";
+
+// import HorizontalLine from "@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js";
+// import HtmlEmbed from "@ckeditor/ckeditor5-html-embed/src/htmlembed.js";
+// import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize.js";
+// import PageBreak from "@ckeditor/ckeditor5-page-break/src/pagebreak.js";
+// import RemoveFormat from "@ckeditor/ckeditor5-remove-format/src/removeformat.js";
+// import SpecialCharacters from "@ckeditor/ckeditor5-special-characters/src/specialcharacters.js";
+// import TableCellProperties from "@ckeditor/ckeditor5-table/src/tablecellproperties";
+// import TableProperties from "@ckeditor/ckeditor5-table/src/tableproperties";
+// import Title from "@ckeditor/ckeditor5-heading/src/title.js";
+// import TodoList from "@ckeditor/ckeditor5-list/src/todolist";
+// import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline.js";
+// import WordCount from "@ckeditor/ckeditor5-word-count/src/wordcount.js";
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
-	Essentials,
-	UploadAdapter,
-	Autoformat,
-	Bold,
-	Italic,
-	BlockQuote,
-	CKFinder,
-	CloudServices,
-	EasyImage,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	Indent,
-	Link,
-	List,
-	MediaEmbed,
-	Paragraph,
-	PasteFromOffice,
-	Table,
-	TableToolbar,
-	TextTransformation,
-	InsertImage,
-	// MathType,
+  // Essentials,
+  // UploadAdapter,
+  // Autoformat,
+  // Bold,
+  // Italic,
+  // BlockQuote,
+  // CKFinder,
+  // EasyImage,
+  // Heading,
+  // Image,
+  // ImageCaption,
+  // ImageStyle,
+  // ImageToolbar,
+  // ImageUpload,
+  // Indent,
+  // Link,
+  // List,
+  // MediaEmbed,
+  // Paragraph,
+  // PasteFromOffice,
+  // Table,
+  // TableToolbar,
+  // TextTransformation,
+  MathType,
+  // Alignment,
+  // CKFinderUploadAdapter,
+  // CloudServices,
+  // Code,
+  // FontBackgroundColor,
+  // FontColor,
+  // FontFamily,
+  // FontSize,
+  // Highlight,
+  // HorizontalLine,
+  // HtmlEmbed,
+  // ImageResize,
+  // PageBreak,
+  // RemoveFormat,
+  // SpecialCharacters,
+  // TableCellProperties,
+  // TableProperties,
+  // Title,
+  // TodoList,
+  // Underline,
+  // WordCount,
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
-	toolbar: {
-		items: [
-			"heading",
-			"|",
-			"bold",
-			"italic",
-			"insertImage",
-			// 'mathType',
-			"link",
-			"bulletedList",
-			"numberedList",
-			"|",
-			"outdent",
-			"indent",
-			"|",
-			"uploadImage",
-			"blockQuote",
-			"insertTable",
-			"mediaEmbed",
-			"undo",
-			"redo",
-		],
-	},
-	image: {
-		toolbar: [
-			"imageStyle:full",
-			"imageStyle:side",
-			"|",
-			"imageTextAlternative",
-		],
-	},
-	table: {
-		contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
-	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: "en",
+  toolbar: {
+    items: [
+      // "heading",
+      // "|",
+      // "alignment",
+      // "bold",
+      // "italic",
+      "mathType",
+      // "link",
+      // "bulletedList",
+      // "numberedList",
+      // "|",
+      // "outdent",
+      // "indent",
+      // "|",
+      // "uploadImage",
+      // "blockQuote",
+      // "insertTable",
+      // "mediaEmbed",
+      // "undo",
+      // "redo",
+    ],
+  },
+  image: {
+    toolbar: [
+      "imageStyle:full",
+      "imageStyle:side",
+      "|",
+      "imageTextAlternative",
+    ],
+  },
+  table: {
+    contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+  },
+  // This value must be kept in sync with the language defined in webpack.config.js.
+  language: "en",
 };
